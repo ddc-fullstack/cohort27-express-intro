fc4ec1fd5bf1be1527daedea22501af514a5dd37
gkephart <gkephart@cnm.edu>
2020-03-24 15:29:49 -0600

    added Class example file

---
+++ typescript/code-samples/class.ts
@@ -0,0 +1,18 @@
+class Foo {
+	private fooId;
+	private fooValue;
+	constructor(fooId :number, fooValue : string) {
+		console.log(fooId);
+		this.fooId = fooId;
+		this.fooValue = fooValue;
+	}
+}
+
+
+const foo : Foo = new Foo(1, "hello");
+
+function doSomething(foo : Foo) {
+	return {...foo, fooBar: "world"}
+}
+
+doSomething(foo);
