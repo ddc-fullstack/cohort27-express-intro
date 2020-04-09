3b5429fc939be00674ccdb19f2fdccbf3266d96b
gkephart <gkephart@cnm.edu>
2020-04-09 14:03:24 -0600

    adding new files

--- intro-to-express/code-samples/index.js
+++ intro-to-express/code-samples/index.js
@@ -4,8 +4,6 @@ const morgan = require("morgan");
 
 const app = express()
 
-
-
 //app.use allows for different middleware to be brought into Express
 app.use(morgan("dev"))
 app.use(express.json())
@@ -13,10 +11,18 @@ app.use(express.json())
 //how to call to the router for express
 const indexRoute = express.Router();
 
+const indexRouteMiddleware = (request, response, nextFunction) =>{
+  console.log("I got called first");
+  return nextFunction();
+}
+
+const indexRouteMiddleware2 = (request, response, nextFunction) =>{
+console.log("I got called second")
+  response.json({status:200, data: [], message:"Hello from the express world"})
+}
+
 //how to configure a route for express
-indexRoute.route("/").get((request, response, next) =>{
-  return response.json({status:200 })
-})
+indexRoute.route("/").get(indexRouteMiddleware, indexRouteMiddleware2);
 
 app.use("/apis", indexRoute)
 
